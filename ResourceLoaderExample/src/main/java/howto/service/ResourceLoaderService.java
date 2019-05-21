77b07356dace9624842bfc72c877b817d2edd966
Santosh Chandak <santoshchandak@gmail.com>
2019-05-21 12:07:29 +0530

    Added javadoc

--- ResourceLoaderExample/src/main/java/howto/service/ResourceLoaderService.java
+++ ResourceLoaderExample/src/main/java/howto/service/ResourceLoaderService.java
@@ -18,8 +18,9 @@ public class ResourceLoaderService implements ResourceLoaderAware {
 
 	/*
 	 * This method reads the resource from root path. The method getResource()
-	 * accepts string value. In "file:pom.xml" no path is given so it considers the
+	 * accepts string value. In "file:pom.xml", no path is given so it considers the
 	 * root as path.
+	 * @throws IOException
 	 */
 	public void showResourceDataUsingRoot() throws IOException {
 		Resource resource = resourceLoader.getResource("file:pom.xml");
@@ -39,8 +40,9 @@ public class ResourceLoaderService implements ResourceLoaderAware {
 
 	/*
 	 * This method reads the resource from file path. As we have given the absolute
-	 * path the ResourceLoader loads the file from that location. Please make sure
-	 * that you have "test.txt" file in your "d:" Local Disk.
+	 * path the ResourceLoader loads the file from that location. 
+	 * Ensure to modify the path "file:d:/test.txt" according to your local storage
+	 * @throws IOException
 	 */
 	public void showResourceDataUsingFilePath() throws IOException {
 		Resource resource = resourceLoader.getResource("file:d:/test.txt");
@@ -62,6 +64,7 @@ public class ResourceLoaderService implements ResourceLoaderAware {
 	 * This method reads the resource from class path. Using "classpath"
 	 * ResourceLoader loads the file from the thread context having resources
 	 * folder.
+	 * @throws IOException
 	 */
 	public void showResourceDataUsingClassPath() throws IOException {
 		Resource resource = resourceLoader.getResource("classpath:test.txt");
@@ -82,6 +85,7 @@ public class ResourceLoaderService implements ResourceLoaderAware {
 	/*
 	 * This method reads the resource from URL. ResourceLoader loads file via URL such
 	 * as file, an HTTP target, an FTP target, etc.
+	 * @throws IOException
 	 */
 	public void showResourceDataUsingURL() throws IOException {
 		Resource resource = resourceLoader.getResource("https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/sha1.js");
