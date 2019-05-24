ab9ad7d7337a2d9cca6f194e79b29ee7fed2a3df
Santosh Chandak <santoshchandak@gmail.com>
2019-05-24 16:39:47 +0530

    Converted to Spring Boot ResourceLoaderService declared as bean/component

--- resoruceloaderdemo/src/main/java/guru/springframework/resourceloaderdemo/service/ResourceLoaderService.java
+++ resoruceloaderdemo/src/main/java/guru/springframework/resourceloaderdemo/service/ResourceLoaderService.java
@@ -1,14 +1,15 @@
-package howto.service;
+package guru.springframework.resourceloaderdemo.service;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
 import org.springframework.context.ResourceLoaderAware;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
 
+@Component("resourceLoaderService")
 public class ResourceLoaderService implements ResourceLoaderAware {
 	private ResourceLoader resourceLoader;
 
