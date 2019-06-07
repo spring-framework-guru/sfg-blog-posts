e590f876e40308d4d3f42b225df9b508ae2c4707
DanielW <DanielW@lucanet.com>
2019-06-07 15:42:50 +0200

    [issue#71]: init spring boot ehcache demo project

---
+++ spring-boot-ehcache/README.md
@@ -0,0 +1,3 @@
+# Using EhCache in Spring Boot
+
+Example project for blog post about using EhCache in Spring Boot applications.
---
+++ spring-boot-ehcache/src/main/java/guru/springframework/ehcache/SpringBootEhCacheDemoApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.ehcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEhCacheDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootEhCacheDemoApplication.class, args);
+  }
+
+}
