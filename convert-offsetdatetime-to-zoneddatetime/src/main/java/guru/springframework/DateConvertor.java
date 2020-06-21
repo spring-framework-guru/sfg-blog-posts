96f129b0dd4e5072f02d9b31e20ec10b3b75fccb
ximanta <ximanta.sarma@gmail.com>
2020-06-21 11:53:53 +0530

    modified code for convert-offsetdatetime-to-zoneddatetime module

--- convert-offsetdatetime-to-zoneddatetime/src/main/java/guru/springframework/DateConvertor.java
+++ convert-offsetdatetime-to-zoneddatetime/src/main/java/guru/springframework/DateConvertor.java
@@ -6,27 +6,23 @@ import java.time.ZonedDateTime;
 
 public class DateConvertor {
     ZoneId zoneId = ZoneId.of("Asia/Kolkata");
-    OffsetDateTime offsetDateTime;
-
-    public OffsetDateTime getDateTimeWithOffset() {
-        offsetDateTime = OffsetDateTime.now();
-        System.out.println(offsetDateTime);
-        return offsetDateTime;
-    }
 
     public ZonedDateTime convertToZonedDateTime() {
+        OffsetDateTime offsetDateTime = OffsetDateTime.now();
         ZonedDateTime zonedDateTime1 = offsetDateTime.toZonedDateTime();
         System.out.println(zonedDateTime1);
         return zonedDateTime1;
     }
 
     public ZonedDateTime convertToAtZoneSameInstant() {
+        OffsetDateTime offsetDateTime = OffsetDateTime.now();
         ZonedDateTime zonedDateTime2 = offsetDateTime.atZoneSameInstant(zoneId);
         System.out.println(zonedDateTime2);
         return zonedDateTime2;
     }
 
     public ZonedDateTime convertToAtZoneSimilarLocal() {
+        OffsetDateTime offsetDateTime = OffsetDateTime.now();
         ZonedDateTime zonedDateTime3 = offsetDateTime.atZoneSimilarLocal(zoneId);
         System.out.println(zonedDateTime3);
         return zonedDateTime3;
