4d1092c71714e90bda0cb1fca3e8726b28ae0059
ximanta <ximanta.sarma@gmail.com>
2020-06-21 12:12:15 +0530

    modified code for convert-offsetdatetime-to-zoneddatetime module

--- convert-offsetdatetime-to-zoneddatetime/src/main/java/guru/springframework/service/DateConverter.java
+++ convert-offsetdatetime-to-zoneddatetime/src/main/java/guru/springframework/service/DateConverter.java
@@ -3,11 +3,7 @@ package guru.springframework.service;
 import java.time.ZonedDateTime;
 
 public interface DateConverter {
-
     public ZonedDateTime convertToZonedDateTime();
-
     public ZonedDateTime convertToAtZoneSameInstant();
-
     public ZonedDateTime convertToAtZoneSimilarLocal();
-
 }
