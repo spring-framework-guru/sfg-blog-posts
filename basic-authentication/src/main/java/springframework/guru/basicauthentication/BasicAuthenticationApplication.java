2e91b30151732e39da09c8432abd81fbefd84883
Ximanta <ximanta.sarma@gmail.com>
2021-01-03 15:03:16 +0530

    Initial commit for basic-authentication

---
+++ basic-authentication/src/main/java/springframework/guru/basicauthentication/BasicAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package springframework.guru.basicauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAuthenticationApplication.class, args);
+	}
+
+}
